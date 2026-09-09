412c3153652aa57f5717d94d2cf2ade85b1e78b4
lkhtty <33829657+lkhtty@users.noreply.github.com>
2026-09-09 18:07:49 +0800

    Switch to local streaming mode and disable overseas DNS
    Modified to disable overseas server polling and use local streaming mode.

--- app/src/main/java/net/programmierecke/radiodroid2/RadioBrowserServerManager.java
+++ app/src/main/java/net/programmierecke/radiodroid2/RadioBrowserServerManager.java
@@ -2,74 +2,38 @@ package net.programmierecke.radiodroid2;
 
 import android.util.Log;
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-import java.util.Random;
-import java.util.Vector;
-
 /**
  * Created by segler on 15.02.18.
+ * Modified: 切断海外服务器轮询，纯本地流媒体模式
  */
-
 public class RadioBrowserServerManager {
-    static String currentServer = null;
-    static String[] serverList = null;
+    static String currentServer = "127.0.0.1";
+    static String[] serverList = new String[]{"127.0.0.1"};
 
     /**
-     * Blocking: do dns request do get a list of all available servers
+     * 彻底阻断海外 DNS 轮询，不发任何网络请求，秒级返回
      */
     private static String[] doDnsServerListing() {
-        Log.d("DNS", "doDnsServerListing()");
-        Vector<String> listResult = new Vector<String>();
-        try {
-            // add all round robin servers one by one to select them separately
-            InetAddress[] list = InetAddress.getAllByName("all.api.radio-browser.info");
-            for (InetAddress item : list) {
-                // do not use original variable, it could fall back to "all.api.radio-browser.info"
-                String currentHostAddress = item.getHostAddress();
-                InetAddress new_item = InetAddress.getByName(currentHostAddress);
-                Log.i("DNS", "Found: " + new_item.toString() + " -> " + new_item.getCanonicalHostName());
-                String name = item.getCanonicalHostName();
-                if (!name.equals("all.api.radio-browser.info") && !name.equals(currentHostAddress)) {
-                    Log.i("DNS", "Added entry: '" + name+"'");
-                    listResult.add(name);
-                }
-            }
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
-        }
-        if (listResult.size() == 0){
-            // should we inform people that their internet provider is not able to do reverse lookups? (= is shit)
-            Log.w("DNS", "Fallback to de1.api.radio-browser.info because dns call did not work.");
-            listResult.add("de1.api.radio-browser.info");
-        }
-        Log.d("DNS", "doDnsServerListing() Found servers: " + listResult.size());
-        return listResult.toArray(new String[0]);
+        Log.d("DNS", "doDnsServerListing() 已禁用海外解析，使用本地模式");
+        return new String[]{"127.0.0.1"};
     }
 
     /**
-     * Blocking: return current cached server list. Generate list if still null.
+     * 直接返回本地列表，禁止网络刷新
      */
     public static String[] getServerList(boolean forceRefresh){
-        if (serverList == null || serverList.length == 0 || forceRefresh){
-            serverList = doDnsServerListing();
+        if (serverList == null || serverList.length == 0){
+            serverList = new String[]{"127.0.0.1"};
         }
         return serverList;
     }
 
     /**
-     * Blocking: return current selected server. Select one, if there is no current server.
+     * 直接返回本地虚拟服务，不再随机抽取海外节点
      */
     public static String getCurrentServer() {
         if (currentServer == null){
-            String[] serverList = getServerList(false);
-            if (serverList.length > 0){
-                Random rand = new Random();
-                currentServer = serverList[rand.nextInt(serverList.length)];
-                Log.d("SRV", "Selected new default server: " + currentServer);
-            }else{
-                Log.e("SRV", "no servers found");
-            }
+            currentServer = "127.0.0.1";
         }
         return currentServer;
     }
